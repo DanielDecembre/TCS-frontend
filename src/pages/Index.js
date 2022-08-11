b94aeba844b5f635c926fc8901f0eddf97ebd876
daniel decembre <danieldecembre@daniels-iMac.local>
2022-08-11 13:27:30 -0400

    wip:first step

---
+++ src/pages/Index.js
@@ -0,0 +1,6 @@
+const Index = (props) => {
+    return <h1>Index Pages</h1>
+};
+
+
+export default Index;
---
+++ src/pages/Show.js
@@ -0,0 +1,5 @@
+const Show = (props) => {
+    return <h1>Show Pages</h1>
+};
+
+export default  Show;
