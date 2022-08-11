cb9cb978896717e3aa05bbc77a5391f23a2a40e6
daniel decembre <danieldecembre@daniels-iMac.local>
2022-08-11 16:32:21 -0400

    wip:styles sasss

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+
+
 const Header = (props) => {
-    return <h1>Header Component</h1>
+    return (
+        <nav className="nav">
+            <Link to="/">
+                <div>Member App</div>
+            </Link>
+        </nav>
+    );
 };
 
-export default Header;
+export default Header;  
